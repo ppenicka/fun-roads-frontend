04637534d486cddd6fc988ac3371b4f3ab450ed1
Petr Penicka <petr.penicka@gmail.com>
2024-12-21 00:08:09 +0700

    feat: initial under construction

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <strong class="text-black text-5xl underline font-bold mt-8"
+      >fun-roads.com</strong
+    >
+    <img
+      src="https://media.tenor.com/FUYuYrQNIxAAAAAM/construction-lol.gif"
+      alt="under construction"
+      class="w-10/12 md:w-1/2 lg:w-1/3 my-auto"
+    />
+  </div>
+</template>
