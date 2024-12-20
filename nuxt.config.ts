a1b29960cd5850d1daf37a9032f69a6832519f03
Petr Penicka <petr.penicka@gmail.com>
2024-12-21 00:36:23 +0700

    feat: add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  modules: ["nuxt-gtag"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+
+  gtag: {
+    id: "G-8RZ1QD4Y0R",
   },
 });
